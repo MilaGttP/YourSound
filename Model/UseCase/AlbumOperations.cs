c466e660dbfb5508d3a1730d2b3ac6dd72851c81
Mila <milaivanova2609@gmail.com>
2023-05-01 21:09:44 +0300

    Added album operations

---
+++ Model/UseCase/AlbumOperations.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YourSound
+{
+    public static class AlbumOperations
+    {
+        public static async Task<List<Album>> GetAlbumsByGenre(string genre)
+        {
+            try
+            {
+                using (var dbContext = new DBContext())
+                {
+                    var albums = await dbContext.Album
+                        .Where(al => al.Genre == genre)
+                        .ToListAsync();
+
+                    return albums;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
