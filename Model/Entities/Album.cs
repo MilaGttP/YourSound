4e9b2e9c1207941059c5a4d1280d9f974296496b
Mila <milaivanova2609@gmail.com>
2023-05-01 19:09:25 +0300

    Added year to album

--- Model/Entities/Album.cs
+++ Model/Entities/Album.cs
@@ -6,12 +6,14 @@ namespace YourSound
         public int ID { get; set; }
         public string Name { get; set; }
         public byte[] Image { get; set; }
+        public int Year { get; set; }
 
         public Album() 
         { 
             ID = 0;
             Name = string.Empty;
             Image = new byte[0];
+            Year = 0;
         }
     }
 }
