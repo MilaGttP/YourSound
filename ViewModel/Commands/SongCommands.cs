af9aa12b2f7969f50a107417c80151bef1a1c0d8
Mila <milaivanova2609@gmail.com>
2023-07-03 14:14:59 +0300

    updated vm

--- ViewModel/Commands/SongCommands.cs
+++ ViewModel/Commands/SongCommands.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace YourSound
 {
     public class SongCommands
     {
-
+        public void LikeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // передача current song та виклик методу SongOperations.IncreasePopularity
+        }
     }
 }
--- ViewModel/GeneralViewModel.cs
+++ ViewModel/GeneralViewModel.cs
@@ -1,13 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace YourSound
 {
-    public class GeneralViewModel
+    public class GeneralViewModel : INotifyPropertyChanged
     {
+        public GeneralViewModel() 
+        {
+            GetSongs();
+        }
 
+        private List<SongAndSinger> topSongs;
+        public List<SongAndSinger> TopSongs
+        {
+            get { return topSongs; }
+            set
+            {
+                topSongs = value;
+                OnPropertyChanged(nameof(TopSongs));
+            }
+        }
+
+        private List<SongAndSinger> youWillLikeSongs;
+        public List<SongAndSinger> YouWillLikeSongs
+        {
+            get { return youWillLikeSongs; }
+            set
+            {
+                youWillLikeSongs = value;
+                OnPropertyChanged(nameof(YouWillLikeSongs));
+            }
+        }
+
+        private List<SongAndSinger> playerSongs;
+        public List<SongAndSinger> PlayerSongs
+        {
+            get { return playerSongs; }
+            set
+            {
+                playerSongs = value;
+                OnPropertyChanged(nameof(PlayerSongs));
+            }
+        }
+
+        private Song selectedSong;
+        public Song SelectedSong
+        {
+            get { return selectedSong; }
+            set
+            {
+                selectedSong = value;
+                OnPropertyChanged(nameof(SelectedSong));
+            }
+        }
+
+        private async void GetSongs()
+        {
+            TopSongs = await SongOperations.GetForTopSongsBorder(6);
+            YouWillLikeSongs = await SongOperations.GetRandomSongs(2);
+            PlayerSongs = await SongOperations.GetRandomSongs(5);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
